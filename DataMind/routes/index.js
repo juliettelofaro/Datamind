adb86b5e6c810b662687fdcd2d592cc8af15e1ae
juliette <juliette.lofaro@gmail.com>
2018-12-06 15:47:36 +0100

    Creation all pages

--- DataMind/routes/index.js
+++ DataMind/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page.*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home');
+});
+
+router.get('/breathe', function(req, res, next) {
+      res.render('breathe');
+});
+
+router.get('/study', function(req, res, next) {
+      res.render('study');
+});
+
+router.get('/gain', function(req, res, next) {
+      res.render('gain');
 });
 
 
