293615e3205eee588f7c47eeb599bd1990e44e4d
juliette <juliette.lofaro@gmail.com>
2018-12-06 21:26:49 +0100

    ajax call

--- DataMind/routes/index.js
+++ DataMind/routes/index.js
@@ -1,25 +1,23 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
 /* GET home page.*/
-router.get('/home', function(req, res, next) {
+router.get('/home', function (req, res, next) {
       console.log("on est dans home");
-  res.render('home');
+      res.render('home');
 });
 
-router.get('/breathe', function(req, res, next) {
+router.get('/breathe', function (req, res, next) {
       console.log("on est dans breathe");
       res.render('breathe');
 });
 
-router.get('/study', function(req, res, next) {
+router.get('/study', function (req, res, next) {
       res.render('study');
 });
 
-router.get('/gain', function(req, res, next) {
+router.get('/gain', function (req, res, next) {
       res.render('gain');
 });
-
-
 
 module.exports = router;
