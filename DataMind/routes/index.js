3b49b94c5fb4de61e07c1c1374f5eb36a031da36
juliette <juliette.lofaro@gmail.com>
2018-12-07 15:31:03 +0100

    Layout and views with {{}}

--- DataMind/routes/index.js
+++ DataMind/routes/index.js
@@ -4,20 +4,23 @@ var router  = express.Router();
 /* GET home page.*/
 router.get('/home', function (req, res, next) {
       console.log("on est dans home");
-      res.render('home');
+      res.render('home', { title: 'Home Page' });
 });
 
 router.get('/breathe', function (req, res, next) {
       console.log("on est dans breathe");
-      res.render('breathe');
+      res.render('breathe', { title: 'Breathe' });
 });
 
 router.get('/study', function (req, res, next) {
-      res.render('study');
+      res.render('study', { title: 'Study' });
 });
 
 router.get('/gain', function (req, res, next) {
-      res.render('gain');
+      res.render('gain', { title: 'Gain' });
 });
 
+router.get('/test', function (req, res, next) {
+      res.render('test');
+});
 module.exports = router;
