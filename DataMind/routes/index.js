00630c806aab26729301ca8cb45c0dc09ae1324c
juliette <juliette.lofaro@gmail.com>
2018-12-07 19:58:47 +0100

    setting the routes

--- DataMind/routes/index.js
+++ DataMind/routes/index.js
@@ -2,25 +2,28 @@ var express = require('express');
 var router  = express.Router();
 
 /* GET home page.*/
-router.get('/home', function (req, res, next) {
-      console.log("on est dans home");
-      res.render('home', { title: 'Home Page' });
+router.get('/', function (req, res, next) {
+      res.render('home', { title: 'Breathe, Chill, Enjoy'});
 });
 
 router.get('/breathe', function (req, res, next) {
-      console.log("on est dans breathe");
+      console.log(" BREATH API")
       res.render('breathe', { title: 'Breathe' });
 });
 
 router.get('/study', function (req, res, next) {
-      res.render('study', { title: 'Study' });
+      res.render('study', { title: 'Study'});
 });
 
-router.get('/gain', function (req, res, next) {
+
+router.get('/gain/:nom', function (req, res, next) {
+      console.log(" GAIN on est dans le controller gain");
+      console.log(" GAIN voici mon prénom ",req.params.nom);
+
       res.render('gain', { title: 'Gain' });
 });
 
-router.get('/test', function (req, res, next) {
-      res.render('test');
-});
+
+
+
 module.exports = router;
