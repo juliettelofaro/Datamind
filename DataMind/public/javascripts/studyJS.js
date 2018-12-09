12dcd9e161771adac5138a2dc653f7b4ea19fae3
juliette <juliette.lofaro@gmail.com>
2018-12-09 14:34:47 +0100

    error messages in study

--- DataMind/public/javascripts/studyJS.js
+++ DataMind/public/javascripts/studyJS.js
@@ -17,18 +17,26 @@ var timerVar;
 
 function countUp() {
       var startSessionButton = document.getElementById("startSessionButton");
-      var stopCountMessage   = document.getElementById("stopCountMessage");
+      var beforeGiveUpMsg = document.getElementById("beforeGiveUpMsg");
+      var giveUpMsg = document.getElementById("giveUpMsg")
       startSessionButton.addEventListener("click", function (e) {
             startSessionButton.disabled = true;
+            giveUpMsg.style.display = "none";
             timerVar            = setInterval(countGo, 1000);
             var buttonStopStudy = document.getElementById("buttonStopStudy");
             console.log("on est dans countup et voici le bouton " + e.target);
             buttonStopStudy.style.display = "block";
+            buttonStopStudy.addEventListener("mouseenter", function() {
+                  beforeGiveUpMsg.style.display = "block";
+            })
+            buttonStopStudy.addEventListener("mouseout", function() {
+                  beforeGiveUpMsg.style.display = "none";
+            })
             buttonStopStudy.addEventListener("click", function () {
                   startSessionButton.disabled = false;
                   totalSeconds = 0;
                   clearInterval(timerVar);
-                  stopCountMessage.innerHTML = "You just gave up, you need to click on the Start button again to start a new session from zero. ";
+                  giveUpMsg.style.display = "block";
             });
       });
 }
@@ -39,13 +47,13 @@ function countGo() {
       var hour   = Math.floor(totalSeconds / 3600);
       var minute = Math.floor((totalSeconds - hour * 3600) / 60);
       var second = totalSeconds - (hour * 3600 + minute * 60);
-      if (second == 10) {
+      if (second == 11) {
             //Bravo vous avez réussi, voici une étoile ! counter disparaît et l'étoile est créé -> studyJS est appelé
             alert("Congratulations ! You won one star ! You'll now be redirected to your Star Page :)");
             totalSeconds = 0;
             studyJS();
             clearInterval(timerVar);
       }
-      document.getElementById("paraStudy").innerHTML = hour + ":" + minute + ":" + second;
+      document.getElementById("paraStudy").innerHTML = minute + ":" + second;
 }
 
