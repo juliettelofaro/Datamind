1bf712bdba82b733552aa5aee19061ee505a2651
juliette <juliette.lofaro@gmail.com>
2018-12-09 15:43:14 +0100

    button Give up centered by putting it in a div wich has display none

--- DataMind/public/javascripts/studyJS.js
+++ DataMind/public/javascripts/studyJS.js
@@ -17,43 +17,52 @@ var timerVar;
 
 function countUp() {
       var startSessionButton = document.getElementById("startSessionButton");
-      var beforeGiveUpMsg = document.getElementById("beforeGiveUpMsg");
-      var giveUpMsg = document.getElementById("giveUpMsg")
+      var beforeGiveUpMsg    = document.getElementById("beforeGiveUpMsg");
+      var giveUpMsg          = document.getElementById("giveUpMsg");
       startSessionButton.addEventListener("click", function (e) {
             startSessionButton.disabled = true;
-            giveUpMsg.style.display = "none";
-            timerVar            = setInterval(countGo, 1000);
-            var buttonStopStudy = document.getElementById("buttonStopStudy");
+            giveUpMsg.style.display     = "none";
+            timerVar                    = setInterval(countGo, 1000);
+
             console.log("on est dans countup et voici le bouton " + e.target);
-            buttonStopStudy.style.display = "block";
-            buttonStopStudy.addEventListener("mouseenter", function() {
-                  beforeGiveUpMsg.style.display = "block";
-            })
-            buttonStopStudy.addEventListener("mouseout", function() {
-                  beforeGiveUpMsg.style.display = "none";
-            })
-            buttonStopStudy.addEventListener("click", function () {
-                  startSessionButton.disabled = false;
-                  totalSeconds = 0;
-                  clearInterval(timerVar);
-                  giveUpMsg.style.display = "block";
-            });
+
       });
 }
 
 function countGo() {
-      console.log("countGo, on rentre ds la fonction, totalSeconds = " + totalSeconds);
       ++totalSeconds;
-      var hour   = Math.floor(totalSeconds / 3600);
-      var minute = Math.floor((totalSeconds - hour * 3600) / 60);
-      var second = totalSeconds - (hour * 3600 + minute * 60);
-      if (second == 11) {
-            //Bravo vous avez réussi, voici une étoile ! counter disparaît et l'étoile est créé -> studyJS est appelé
+      var buttonStopStudy = document.getElementById("buttonStopStudy");
+      var paraStudy       = document.getElementById("paraStudy");
+      var hour            = Math.floor(totalSeconds / 3600);
+      var minute          = Math.floor((totalSeconds - hour * 3600) / 60);
+      var second          = totalSeconds - (hour * 3600 + minute * 60);
+      if (minute == 2) {
             alert("Congratulations ! You won one star ! You'll now be redirected to your Star Page :)");
             totalSeconds = 0;
             studyJS();
             clearInterval(timerVar);
       }
-      document.getElementById("paraStudy").innerHTML = minute + ":" + second;
+      if (minute < 10 && second < 10) {
+            paraStudy.innerHTML = "0" + minute + ":" + "0" + second;
+      } else if (minute > 10 && second < 10) {
+            paraStudy.innerHTML = minute + ":" + "0" + second;
+      } else if (minute < 10 && second >= 10) {
+            paraStudy.innerHTML = "0" + minute + ":" + second;
+      } else {
+            paraStudy.innerHTML = minute + ":" + second;
+      }
+      divButtonStopStudy.style.display = "block";
+      buttonStopStudy.addEventListener("mouseenter", function () {
+            beforeGiveUpMsg.style.display = "block";
+      });
+      buttonStopStudy.addEventListener("mouseout", function () {
+            beforeGiveUpMsg.style.display = "none";
+      });
+      buttonStopStudy.addEventListener("click", function () {
+            startSessionButton.disabled = false;
+            totalSeconds                = 0;
+            clearInterval(timerVar);
+            giveUpMsg.style.display = "block";
+      });
 }
 
