1c8a5d0d4b241c71668d29ea7c6fc80f53b72101
juliette <juliette.lofaro@gmail.com>
2018-12-10 16:43:14 +0100

    work, the circle grows up and down again and again

--- DataMind/public/javascripts/breatheJS.js
+++ DataMind/public/javascripts/breatheJS.js
@@ -13,43 +13,41 @@ function circlecanvasBreathe() {
             c.arc(600, 200, radius, 0, Math.PI * 2);
             c.strokeStyle = "red";
             c.stroke();
-            if (radius > 49 && radius <= 199) {
-                  radius += 1;
-                  console.log("1. ANIMATE IF");
-                  console.log("ANIMATE bigRadius dans animate : " + bigRadius);
-            } else if (radius === 200) {
-                  console.log("2. ANIMATE  ELSE IF");
+            if (radius >= 50 && radius <= 200) {
+                  radius += 0.7;
+                  console.log("1. radius : " + radius + ", bigRadius" + bigRadius);
+            }
+            if (radius >= 200 && radius <= 201) {
+                  console.log("2. radius : " + radius + ", bigRadius" + bigRadius);
                   window.cancelAnimationFrame(myReq);
                   goesBack();
+                  return radius;
+            } else if (bigRadius <= 50 && bigRadius <= 200) {
+                  console.log("3. radius : " + radius + ", bigRadius" + bigRadius);
+                  goesBack();
             }
+
       }
 
       function goesBack() {
-            if (bigRadius >= 49 && bigRadius <= 51) {
-                  bigRadius = 200;
-            }
+
+            console.log(bigRadius);
             var goesBackRequest = requestAnimationFrame(goesBack);
             c.clearRect(0, 0, innerWidth, innerHeight);
             c.beginPath();
-            c.arc(600, 200, bigRadius, 0, Math.PI * 2);
+            c.arc(600, 200, radius, 0, Math.PI * 2);
             c.strokeStyle = "red";
             c.stroke();
-            //le pb,c qu'une fois qu'on passe une fois ici, bigRadius reste à 51, donc
-            //la 2e fois qu'on passe il est encore à 50
-            // donc le cercle se creer à 50,
-            //ne passe pas dans la boucle ci-dessous
-            //il faut que ce soit 50 ici
-            //mais quand je mets 50 animate() n'est pas rappelé
-            if (bigRadius <= 200 && bigRadius >= 50) {
-                  console.log("3. GOESBACK IF");
-                  console.log("3. GOESBACK bigRadius dans animate : " + bigRadius);
-                  bigRadius -= 0.5;
-            } else if (bigRadius === 50) {
-                  console.log("4. GOESBACK  ELSE IF");
-                  console.log("4 GOESBACK bigRadius dans animate : " + bigRadius);
-                  radius = bigRadius;
+            if (radius <= 201 && radius >= 51) {
+                  console.log("4GO. radius : " + radius + ", radius" + radius);
+                  radius -= 1;
+            } else if (radius <= 52) {
+                  console.log("5GO. radius : " + radius + ", bigRadius" + bigRadius);
+
+
                   window.cancelAnimationFrame(goesBackRequest);
                   animate();
+                  return radius;
             }
       }
 
@@ -57,5 +55,3 @@ function circlecanvasBreathe() {
             animate();
       });
 }
-
-
