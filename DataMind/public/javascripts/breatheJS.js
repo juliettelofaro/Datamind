b2e740fa2091b9a181875e0862cb19842c05e63e
juliette <juliette.lofaro@gmail.com>
2018-12-10 18:46:00 +0100

    cleaner code, added more var

--- DataMind/public/javascripts/breatheJS.js
+++ DataMind/public/javascripts/breatheJS.js
@@ -1,25 +1,31 @@
 function circlecanvasBreathe() {
-      var horn             = new Audio('images/mymusic.mp3');
-      var canvasBreathe    = document.getElementById("canvasBreathe");
-      canvasBreathe.height = 450;
-      canvasBreathe.width  = window.innerWidth;
-      var c                = canvasBreathe.getContext("2d");
-      var radius           = 50;
+      var horn                 = new Audio('images/mymusic.mp3');
+      var canvasBreathe        = document.getElementById("canvasBreathe");
+      canvasBreathe.height     = 450;
+      canvasBreathe.width      = window.innerWidth;
+      var c                    = canvasBreathe.getContext("2d");
+      var radius               = 50;
       var buttonStartBreathing = document.getElementById("buttonStartBreathing");
+      var x                    = 625;
+      var y                    = 200;
+      var xText                = 600;
+      var yText                = 210;
+      var deeppink             = "deeppink";
+      var lightpink            = "lightpink";
 
       function animate() {
             var myReq = requestAnimationFrame(animate);
             c.clearRect(0, 0, innerWidth, innerHeight);
             c.beginPath();
-            c.arc(625, 200, radius, 0, Math.PI * 2);
-            c.fillText("Breathe In",600,210);
-            c.fillStyle = "deeppink";
-            c.strokeStyle = "lightpink";
+            c.arc(x, y, radius, 0, Math.PI * 2);
+            c.fillText("Breathe In", xText, yText);
+            c.fillStyle   = deeppink;
+            c.strokeStyle = lightpink;
             c.stroke();
-            if (radius >= 50 && radius <= 200) {
+            if (radius >= 50 && radius <= y) {
                   radius += 0.6;
             }
-            if (radius >= 200 && radius <= 201) {
+            if (radius >= y && radius <= 201) {
                   window.cancelAnimationFrame(myReq);
                   goesBack();
                   return radius;
@@ -30,10 +36,10 @@ function circlecanvasBreathe() {
             var goesBackRequest = requestAnimationFrame(goesBack);
             c.clearRect(0, 0, innerWidth, innerHeight);
             c.beginPath();
-            c.arc(625, 200, radius, 0, Math.PI * 2);
-            c.fillText("Breathe Out",600,210);
-            c.fillStyle = "lightpink";
-            c.strokeStyle = "deeppink";
+            c.arc(x, y, radius, 0, Math.PI * 2);
+            c.fillText("Breathe Out", xText, yText);
+            c.fillStyle   = lightpink;
+            c.strokeStyle = deeppink;
             c.stroke();
             if (radius <= 201 && radius >= 51) {
                   radius -= 0.4;
