f909d6b6787432491793b250adad050b8f8b762b
juliette <juliette.lofaro@gmail.com>
2018-12-10 17:10:39 +0100

    clean code for circles

--- DataMind/public/javascripts/breatheJS.js
+++ DataMind/public/javascripts/breatheJS.js
@@ -4,7 +4,6 @@ function circlecanvasBreathe() {
       canvasBreathe.width  = window.innerWidth;
       var c                = canvasBreathe.getContext("2d");
       var radius           = 50;
-      var bigRadius        = 200;
 
       function animate() {
             var myReq = requestAnimationFrame(animate);
@@ -14,24 +13,16 @@ function circlecanvasBreathe() {
             c.strokeStyle = "red";
             c.stroke();
             if (radius >= 50 && radius <= 200) {
-                  radius += 0.7;
-                  console.log("1. radius : " + radius + ", bigRadius" + bigRadius);
+                  radius += 0.6;
             }
             if (radius >= 200 && radius <= 201) {
-                  console.log("2. radius : " + radius + ", bigRadius" + bigRadius);
                   window.cancelAnimationFrame(myReq);
                   goesBack();
                   return radius;
-            } else if (bigRadius <= 50 && bigRadius <= 200) {
-                  console.log("3. radius : " + radius + ", bigRadius" + bigRadius);
-                  goesBack();
             }
-
       }
 
       function goesBack() {
-
-            console.log(bigRadius);
             var goesBackRequest = requestAnimationFrame(goesBack);
             c.clearRect(0, 0, innerWidth, innerHeight);
             c.beginPath();
@@ -39,12 +30,8 @@ function circlecanvasBreathe() {
             c.strokeStyle = "red";
             c.stroke();
             if (radius <= 201 && radius >= 51) {
-                  console.log("4GO. radius : " + radius + ", radius" + radius);
-                  radius -= 1;
+                  radius -= 0.2;
             } else if (radius <= 52) {
-                  console.log("5GO. radius : " + radius + ", bigRadius" + bigRadius);
-
-
                   window.cancelAnimationFrame(goesBackRequest);
                   animate();
                   return radius;
